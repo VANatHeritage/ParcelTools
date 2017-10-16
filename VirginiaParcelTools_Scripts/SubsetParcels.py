450857c366ddfd27c04d8954548de335842b47d0
Kirsten Hazler <kirsten.hazler@dcr.virginia.gov>
2017-10-16 11:08:59 -0400

    allow feature class input
    Edit to allow feature class input; added messaging; free-standing script

--- VirginiaParcelTools_Scripts/SubsetParcels.py
+++ VirginiaParcelTools_Scripts/SubsetParcels.py
@@ -2,8 +2,9 @@
 # SubsetParcels.py
 # Version:  Python 2.7.5
 # Creation Date: 2017-06-21
-# Last Edit: 2017-06-28
+# Last Edit: 2017-10-16
 # Creator:  Roy Gilb and DJ Helkowski
+# Edited by Kirsten Hazler
 #
 #  Notes:
 
@@ -28,33 +29,42 @@ outGDB = arcpy.GetParameterAsText(2)       #GDB to store the output feature clas
 arcpy.env.workspace = inLocalGDB
 tables = arcpy.ListTables()
 
+#Make a feature layer
+arcpy.MakeFeatureLayer_management (inParcels, "lyrParcels")
 
 for locTable in tables: 
-	try: 
-		#set up some variables
-		locName = os.path.basename(locTable)[4:-9]
-		arcpy.AddMessage(locName)
-		
-		#Join the current table to the parcels layer
-		arcpy.AddJoin_management (inParcels, "VGIN_QPID", locTable, "VGIN_QPID", "KEEP_COMMON")
-		
-		#Copy the features to a new feature class
-		arcpy.CopyFeatures_management(inParcels, outGDB + os.sep + locName)
-		
-		#Remove the current join
-		arcpy.RemoveJoin_management(inParcels)
-	except:
-		arcpy.AddWarning('Failed to process table %s.' % locTable)
-		
-		#Error Handling code swiped from "A Python Primer for ArcGIS"
-		tb = sys.exc_info()[2]
-		
-		tbinfo = traceback.format_tb(tb)[0]
-		pymsg = "Python Errors:\nTraceback Info:\n" + tbinfo + "\nError Info:\n" +str(sys.exc_info()[1])
-		msgs = "ARCPY ERRORS:\n" + arcpy.GetMessages(2) + "\n"
-		
-		arcpy.AddWarning(msgs)
-		arcpy.AddWarning(pymsg)
-		arcpy.AddMessage(arcpy.GetMessages(1))
+   try: 
+      #Set up some variables
+      locName = os.path.basename(locTable)[4:-9]
+      arcpy.AddMessage("Working on %s..." %locName)
+      
+      #Join the current table to the parcels layer
+      arcpy.AddMessage("Joining attributes")
+      arcpy.AddJoin_management ("lyrParcels", "VGIN_QPID", locTable, "VGIN_QPID", "KEEP_COMMON")
+      
+      #Copy the features to a new feature class
+      arcpy.AddMessage("Copying features")
+      arcpy.CopyFeatures_management("lyrParcels", outGDB + os.sep + locName)
+      
+      #Remove the current join
+      arcpy.AddMessage("Removing join")
+      arcpy.RemoveJoin_management("lyrParcels")
+      
+      arcpy.AddMessage("Completed %s" %locName)
+   except:
+      arcpy.AddWarning('Failed to process table %s.' % locTable)
+      
+      #Error Handling code swiped from "A Python Primer for ArcGIS"
+      tb = sys.exc_info()[2]
+      
+      tbinfo = traceback.format_tb(tb)[0]
+      pymsg = "Python Errors:\nTraceback Info:\n" + tbinfo + "\nError Info:\n" +str(sys.exc_info()[1])
+      msgs = "ARCPY ERRORS:\n" + arcpy.GetMessages(2) + "\n"
+      
+      arcpy.AddWarning(msgs)
+      arcpy.AddWarning(pymsg)
+      arcpy.AddMessage(arcpy.GetMessages(1))
+
+
 
 
