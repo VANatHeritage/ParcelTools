2169441b1ede0dbf8abe224a473a75102d98c24e
David Bucklin <david.bucklin@dcr.virginia.gov>
2023-11-09 15:21:24 -0500

    re-work script

--- VirginiaParcelTools_Scripts/SubsetParcels.py
+++ VirginiaParcelTools_Scripts/SubsetParcels.py
@@ -1,71 +1,85 @@
 # ----------------------------------------------------------------------------------------
 # SubsetParcels.py
-# Version:  Python 2.7.5
+# Version:  Python 3.x
 # Creation Date: 2017-06-21
-# Last Edit: 2021-04-06
-# Creator:  Roy Gilb and DJ Helkowski
-# Edited by Kirsten Hazler
-#
-#  Notes:
+# Last Edit: 2023-11-09
+# Creators:  Roy Gilb, DJ Helkowski, Kirsten Hazler, David Bucklin
 
 # ----------------------------------------------------------------------------------------
 
 # Import required modules
 import arcpy
-from arcpy.sa import *
-arcpy.CheckOutExtension("Spatial")
 import os # provides access to operating system functionality such as file and directory paths
-import sys # provides access to Python system functions
-import traceback # used for error handling
-import gc # garbage collection
-from datetime import datetime # for time-stamping
+import time
 
+def unique_values(table, field):
+   ''' Gets list of unique values in a field.
+   Thanks, ArcPy Cafe! https://arcpy.wordpress.com/2012/02/01/create-a-list-of-unique-field-values/'''
+   with arcpy.da.SearchCursor(table, [field]) as cursor:
+      return sorted({row[0] for row in cursor})
+
+def getFlds(table):
+   flds = [a.name for a in arcpy.ListFields(table)]
+   return flds
+
+arcpy.env.workspace = "in_memory"
+arcpy.env.overwriteOutput = True
+
+# headsup: For interactive use.
+# base_dir = r"C:\David\proc"
+# new_dir = base_dir + os.sep + "VA_Parcels_2023Q2"
+# if not os.path.exists(new_dir):
+#    os.makedirs(new_dir)
+# new_gdb = new_dir + os.sep + 'VA_Parcels3.gdb'
+# # 
+# # Set paths to GDBs from VGIN
+# va_parcels_gdb = r'C:\David\proc\Virginia_Parcel_Dataset_2023Q2.gdb'
+# parcels = va_parcels_gdb + os.sep + "VA_parcels"
+# va_tables_gdb = r'C:\David\proc\Virginia_Parcel_Dataset_LocalSchemas_2023Q2.gdb'
 
 # Script arguments to be input by user
-inParcels = arcpy.GetParameterAsText(0)    #Input parcels layer
-inLocalGDB = arcpy.GetParameterAsText(1)   #Input geodatabase containing the locality tables 
-outGDB = arcpy.GetParameterAsText(2)       #GDB to store the output feature classes
+parcels = arcpy.GetParameterAsText(0)    #Input parcels layer
+va_tables_gdb = arcpy.GetParameterAsText(1)   #Input geodatabase containing the locality tables 
+new_gdb = arcpy.GetParameterAsText(2)       #GDB to store the output feature classes
 
-arcpy.env.workspace = inLocalGDB
-tables = arcpy.ListTables()
+# Make sure field names are correct
+# getFlds(parcels)
+# Set field name variables
+locFld = "LOCALITY"
+idFld = "VGIN_QPID"
 
-# #Make a feature layer
-# arcpy.MakeFeatureLayer_management (inParcels, "lyrParcels")
+# Create new GDB
+if not arcpy.Exists(new_gdb):
+   arcpy.AddMessage("Creating " + new_gdb + "...")
+   arcpy.CreateFileGDB_management(os.path.dirname(new_gdb), os.path.basename(new_gdb))
 
-for locTable in tables: 
-   try: 
-      #Set up some variables
-      locName = os.path.basename(locTable)[4:]
-      arcpy.AddMessage("Working on %s..." %locName)
-      
-      #Make a feature layer
-      attName = locName.replace("_", " ")
-      where_clause = "LOCALITY = '%s'" %attName
-      arcpy.MakeFeatureLayer_management (inParcels, "lyrParcels", where_clause)
-            
-      #Join the current table to the parcels layer
-      arcpy.AddMessage("Joining attributes")
-      arcpy.AddJoin_management ("lyrParcels", "VGIN_QPID", locTable, "VGIN_QPID", "KEEP_COMMON")
-      
-      #Copy the features to a new feature class
-      arcpy.AddMessage("Copying features")
-      arcpy.CopyFeatures_management("lyrParcels", outGDB + os.sep + locName)
-      
-      #Remove the current join
-      arcpy.AddMessage("Removing join")
-      arcpy.RemoveJoin_management("lyrParcels")
-      
-      arcpy.AddMessage("Completed %s" %locName)
-   except:
-      arcpy.AddWarning('Failed to process table %s.' % locTable)
-      
-      #Error Handling code swiped from "A Python Primer for ArcGIS"
-      tb = sys.exc_info()[2]
-      
-      tbinfo = traceback.format_tb(tb)[0]
-      pymsg = "Python Errors:\nTraceback Info:\n" + tbinfo + "\nError Info:\n" +str(sys.exc_info()[1])
-      msgs = "ARCPY ERRORS:\n" + arcpy.GetMessages(2) + "\n"
-      
-      arcpy.AddWarning(msgs)
-      arcpy.AddWarning(pymsg)
-      arcpy.AddMessage(arcpy.GetMessages(1))
+# Get list of localities
+loc_ls = unique_values(parcels, locFld)
+arcpy.SetProgressor("step", "Creating parcel subsets by locality...", 0, len(loc_ls), 1)
+n = 0
+
+# Loop over localities
+for l in loc_ls:
+   # arcpy.AddMessage(l)
+   # Headsup: not using error handling, so user can cancel process.
+   arcpy.SetProgressorLabel("Processing " + l + "...")
+   fc_out = new_gdb + os.sep + l.replace(" ", "_")
+   nm = "TBL_" + l.replace(" ", "_")
+   join_tab = va_tables_gdb + os.sep + nm
+   if arcpy.Exists(fc_out):
+      arcpy.AddMessage(l + " already exists.")
+      continue
+   t0 = time.time()
+   lyr = arcpy.MakeFeatureLayer_management(parcels, where_clause=locFld + " = '" + l + "'")
+   if not arcpy.Exists(join_tab):
+      arcpy.AddMessage(join_tab + " does not exist.")
+   else:
+      arcpy.AddJoin_management(lyr, idFld, join_tab, idFld, "KEEP_ALL")
+   arcpy.ExportFeatures_conversion(lyr, fc_out)
+   t1 = time.time()
+   arcpy.AddMessage("Completed processing for " + l + " in " + str(round(t1 - t0)) + " seconds.")
+   n += 1
+   arcpy.SetProgressorPosition(n)
+arcpy.AddMessage("Compacting geodatabase...")
+arcpy.Compact_management(new_gdb)
+arcpy.AddMessage("Done.")
